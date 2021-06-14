c8298e7208aada7f06388c57aeb594baa9230196
akunadze <krollokot@gmail.com>
2021-06-14 10:53:46 -0700

    README and misc fixes

--- server/keys/genkeys.bat
+++ server/keys/genkeys.bat
@@ -1,7 +1,6 @@
-rem call "C:\Program Files\Git\usr\bin\openssl" genrsa -out MyRootCA.key 2048
-rem call "C:\Program Files\Git\usr\bin\openssl" req -x509 -new -nodes -key MyRootCA.key -sha256 -days 1024 -out MyRootCA.pem 
+@echo off
 
+echo ============ Generating server keys. Input server IP when asked for CN =================
 call "C:\Program Files\Git\usr\bin\openssl" genrsa -out server.key 2048
 call "C:\Program Files\Git\usr\bin\openssl" req -new -key server.key -out server.csr
-
 call "C:\Program Files\Git\usr\bin\openssl" x509 -req -in server.csr -CA MyRootCA.pem -CAkey MyRootCA.key -CAcreateserial -out server.pem -days 1024 -sha256
